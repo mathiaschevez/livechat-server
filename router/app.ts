2ef1ab2b806baa5b1121993db9a5a629448cbbe9
Mathias Chevez <davidchevez90@gmail.com>
2025-03-06 06:01:02 -0700

    added more routes

--- router/app.ts
+++ router/app.ts
@@ -15,3 +15,12 @@ app.use(express.json());
 
 
 app.use('/messages', messagesRouter);
+
+app.get('/', (req, res) => {
+  res.send('Server is running'); // You can customize this message
+});
+
+// Catch-all route for 404 errors (optional)
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});

bcba6aecb47aaaadcf24c38e14daf5ca61b2ed6d
Mathias Chevez <davidchevez90@gmail.com>
2025-03-06 03:02:54 -0700

    idk

--- router/messages/index.ts
+++ router/messages/index.ts
@@ -3,6 +3,14 @@ import { messagesCollection } from '../../mongo/client';
 
 export const messagesRouter = Router();
 
+messagesRouter.post('/', async(req, res) => {
+  const messages = await messagesCollection
+    .find()
+    .toArray();
+
+  res.json(messages);
+})
+
 messagesRouter.post('/insert', async (req, res) => {
   const message = req.body.message;
   messagesCollection.insertOne({ message });

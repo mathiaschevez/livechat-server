3fa2de6a24c0f6969817e7c1c77a7a9eebd03132
Mathias Chevez <davidchevez90@gmail.com>
2025-03-06 06:19:48 -0700

    simplify

--- router/app.ts
+++ router/app.ts
@@ -5,22 +5,8 @@ import cors from 'cors';
 export const app = express();
 
 // Enable CORS for all requests
-app.use(cors({
-  origin: process.env.CLIENT_ORIGIN_URL,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_ORIGIN_URL ?? ''); // Allow the frontend to connect
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specific methods
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
-  res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials (cookies, etc.)
-  next(); // Pass to the next middleware or route handler
-});
-
 
 app.use('/messages', messagesRouter);
 

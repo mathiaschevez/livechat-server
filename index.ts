426dd3cd6aa3730e36b76f41bbfe4434b77e9ab9
Mathias Chevez <davidchevez90@gmail.com>
2025-03-06 03:54:16 -0700

    new mongo client connect

--- index.ts
+++ index.ts
@@ -3,9 +3,8 @@ import http from 'http';
 import { app } from './router/app';
 
 const server = http.createServer(app);
-const PORT = 8000;
 socket.listen(server);
 
-server.listen(PORT, () => {
+server.listen(process.env.PORT, () => {
   console.log('listening on *:8000');
 });
--- mongo/client.ts
+++ mongo/client.ts
@@ -3,7 +3,11 @@ import { config } from 'dotenv';
 
 config();
 
-const client = new MongoClient(process.env.MONGODB_CONNECTION_STRING ?? '');
+const client = new MongoClient(process.env.MONGODB_CONNECTION_STRING ?? '', {
+  replicaSet: 'rs0',
+  retryWrites: true,
+  ignoreUndefined: true
+});
 client.connect().catch(console.error);
 
 const database = client.db('livechat');

bab9b5d3332873ed4fc38bc55aafaa9e265ba44d
Mathias Chevez <davidchevez90@gmail.com>
2025-04-02 00:25:43 -0600

    route for saving and removing saved rankings from user

--- router/users/index.ts
+++ router/users/index.ts
@@ -24,3 +24,25 @@ usersRouter.post('/insert', async (req, res) => {
     res.status(500).json({ message: 'Failed to insert user' });
   }
 });
+
+usersRouter.post('/save-ranking', async (req, res) => {
+  const { userId, rankingId } = req.body;
+
+  try {
+    await usersCollection.findOneAndUpdate({ userId }, { $push: { savedRankings: rankingId }})
+    res.status(201).json({ message: 'Ranking saved successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to save ranking' });
+  }
+})
+
+usersRouter.post('/remove-saved-ranking', async (req, res) => {
+  const { userId, rankingId } = req.body;
+
+  try {
+    await usersCollection.findOneAndUpdate({ userId }, { $pull: { savedRankings: rankingId }})
+    res.status(201).json({ message: 'Ranking un saved successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to un saved ranking' });
+  }
+})
